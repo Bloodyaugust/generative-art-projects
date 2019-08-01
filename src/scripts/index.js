5e8dec47a3e6211cf677e00dc3aa70d4f8d1abfd
Bloodyaugust <greysonrichey@gmail.com>
2019-08-01 12:38:02 -0600

    add dataset

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
 import '../styles/index.scss';
 import * as _ from 'lodash';
 import * as p5 from 'p5';
-// import * as data from './weather.json';
+import * as data from './mavenlink-commit-activity.json';
+
+console.log(data);
 
 const P5 = new p5((sketch) => {
   sketch.setup = () => {
