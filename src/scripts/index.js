8837af75e98c0700b96fd683917ad254634e3b20
Bloodyaugust <greysonrichey@gmail.com>
2019-08-01 14:34:42 -0600

    Commit result of Mavenlink mob presentation session

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,14 +3,37 @@ import * as _ from 'lodash';
 import * as p5 from 'p5';
 import * as data from './mavenlink-commit-activity.json';
 
-console.log(data);
+const commits = data.default;
+const baseLightnessValue = 42;
+const colorScalingValue = 0.15;
 
 const P5 = new p5((sketch) => {
+  const allDays = [];
+
+  _.each(commits, (week) => {
+    _.each(week.days, (day) => {
+      allDays.push(day);
+    });
+  });
+
   sketch.setup = () => {
     sketch.createCanvas(window.innerWidth, window.innerHeight);
   };
 
   sketch.draw = () => {
-    sketch.background(50);
+    let sinScalar = Math.sin(Date.now() / 1000);
+
+    sketch.colorMode(sketch.RGB);
+
+    sketch.background(179, 76, 33);
+    sketch.fill(0, 0, 0, 0);
+
+    sketch.colorMode(sketch.HSL);
+
+    _.each(allDays, (day, i) => {
+      sketch.stroke(18, 69, baseLightnessValue + (day * sinScalar * colorScalingValue), 1);
+
+      sketch.circle(window.innerWidth / 2, window.innerHeight / 2, day + (i * 1.5));
+    });
   };
 });
