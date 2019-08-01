a8e89614f78ebb9b567c79c83627ac2d1f3f96ca
Bloodyaugust <greysonrichey@gmail.com>
2019-08-01 15:28:16 -0600

    Add post-presentation edits

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,8 +4,8 @@ import * as p5 from 'p5';
 import * as data from './mavenlink-commit-activity.json';
 
 const commits = data.default;
-const baseLightnessValue = 42;
-const colorScalingValue = 0.15;
+const baseLightnessValue = 40;
+const colorScalingValue = 20;
 
 const P5 = new p5((sketch) => {
   const allDays = [];
@@ -16,24 +16,28 @@ const P5 = new p5((sketch) => {
     });
   });
 
+  const dayMax = _.max(allDays);
+  const dayMin = _.min(allDays);
+
   sketch.setup = () => {
     sketch.createCanvas(window.innerWidth, window.innerHeight);
   };
 
   sketch.draw = () => {
-    let sinScalar = Math.sin(Date.now() / 1000);
+    let sinScalar = Math.abs(Math.sin(Date.now() / 1000));
 
-    sketch.colorMode(sketch.RGB);
-
-    sketch.background(179, 76, 33);
+    sketch.background(18, 69, 60);
     sketch.fill(0, 0, 0, 0);
 
     sketch.colorMode(sketch.HSL);
 
     _.each(allDays, (day, i) => {
-      sketch.stroke(18, 69, baseLightnessValue + (day * sinScalar * colorScalingValue), 1);
+      let indexedSinScalar = Math.abs(Math.sin((Date.now() / 60000) + i));
+      let dayScalar = day / dayMax;
 
-      sketch.circle(window.innerWidth / 2, window.innerHeight / 2, day + (i * 1.5));
+      sketch.stroke(18, 69, baseLightnessValue - (dayScalar * colorScalingValue), 1);
+
+      sketch.circle(window.innerWidth / 2, window.innerHeight / 2, (day + (i * 1.5)) * indexedSinScalar);
     });
   };
 });
